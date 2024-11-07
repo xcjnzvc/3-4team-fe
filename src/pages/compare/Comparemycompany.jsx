05f3fdba850f0aaf3d23d3e90089696343f11f9d
xcjnzvc <rkdenrjd@gmail.com>
2024-11-07 22:43:45 +0900

    헤더완성

---
+++ src/pages/compare/Comparemycompany.jsx
@@ -0,0 +1,7 @@
+export function CompareMyCompany() {
+  return (
+    <div>
+      <h2 style={{ color: "#fff" }}>성공 (예시입니다! 다시 작업해주세요~)</h2>
+    </div>
+  );
+}
---
+++ src/shared/component/Header.jsx
@@ -0,0 +1,60 @@
+// import styles from "../component/headerModule.css";
+import "../component/header.css";
+import "../css/com.css";
+import { Link, NavLink } from "react-router-dom";
+
+const activeStyle = {
+  color: "#fff",
+  textDecoration: "none",
+};
+
+const defaultColor = {
+  color: "#747474",
+};
+
+export function Header() {
+  return (
+    <header>
+      <div className="gnb">
+        <Link to="/" className="logo">
+          <img src="/img/img_logo.png" />
+        </Link>
+        <ul className="nav">
+          <li>
+            <NavLink
+              to="compare"
+              style={({ isActive }) => {
+                return isActive ? activeStyle : defaultColor;
+              }}
+            >
+              나의 기업 비교
+            </NavLink>
+          </li>
+          <li>
+            {/* 임시경로입니다 수정해야해요! */}
+            <NavLink
+              to="/a"
+              style={({ isActive }) => {
+                return isActive ? activeStyle : defaultColor;
+              }}
+            >
+              비교 현황
+            </NavLink>
+          </li>
+
+          <li>
+            {/* 임시경로입니다 수정해야해요! */}
+            <NavLink
+              to="/s"
+              style={({ isActive }) => {
+                return isActive ? activeStyle : defaultColor;
+              }}
+            >
+              투자 현황
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
