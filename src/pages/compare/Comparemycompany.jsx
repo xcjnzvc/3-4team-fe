0fc2df1ba52b7df1d0604b5acf4f51545b8d64dd
hogeun <originalbae1103@gmail.com>
2024-11-18 10:22:13 +0900

    2-1 프론트 작업

--- src/pages/compare/Comparemycompany.jsx
+++ src/pages/compare/Comparemycompany.jsx
@@ -1,5 +1,25 @@
 import "./comparemycompany.css";
 
 export function CompareMyCompany() {
-  return <div className="inner">//여기서 작업하시면 돼요!!</div>;
-}
+  return (
+    <>
+  <div class="container">
+    <header class="header">
+      <h1 class="header-title">나의 기업을 선택해 주세요!</h1>
+    </header>
+
+    <section class="company-list">
+      <button class="add-button">
+        <img src="/img/btn_plus.png" alt="plus 버튼"/>
+      </button>
+      <span class="add-button-text">기업 추가</span>
+    </section>
+
+    <footer class="footer">
+      <button class="compare-button">
+        <span class="compare-button-text">기업 비교하기</span>
+      </button>
+    </footer>
+  </div>
+    </>
+)}
