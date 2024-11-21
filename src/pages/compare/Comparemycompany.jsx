6778a130b2ce563d61336ed2141daa5329c17524
xcjnzvc <rkdenrjd@gmail.com>
2024-11-21 16:48:02 +0900

    Co-authored-by: Taejin Kang <ehdaydkv112@users.noreply.github.com>

--- src/pages/compare/Comparemycompany.jsx
+++ src/pages/compare/Comparemycompany.jsx
@@ -15,118 +15,6 @@ export const MODAL_TYPES = {
 };
 
 export function CompareMyCompany() {
-
-  const [modalType, setModalType] = useState(null);
-  const {
-    companies,
-    pagination,
-    searchKeyword,
-    selectedCompanies,
-    handlePageChange,
-    handleCompanySelect,
-    setSearchKeyword,
-    setCompanies,
-    fetchCompanies,
-    selectMyCompany,
-    handleClickMyCompany,
-    setSelectMyCompany,
-  } = useCompanyData();
-
-  const handleModalOpen = (type) => setModalType(type);
-  const handleModalClose = () => setModalType(null);
-
-  const handleConfirm = () => {
-    // 선택 확인 로직
-    handleClickMyCompany();
-  };
-
-  const buttonStyles = {
-    color: "white",
-    borderColor: "white",
-  };
-
-  const buttonText = "선택 완료";
-
-  return (
-    <>
-      {modalType === MODAL_TYPES.SELECT && (
-        // <CompareModal />
-        <SelectCompanyModal
-          onClose={handleModalClose}
-          handleConfirm={handleConfirm}
-          companies={companies}
-          pagination={pagination}
-          searchKeyword={searchKeyword}
-          onSearch={setSearchKeyword}
-          onSelectCompany={handleCompanySelect}
-          setSearchKeyword={setSearchKeyword}
-          setCompanies={setCompanies}
-          fetchCompanies={fetchCompanies}
-          handlePageChange={handlePageChange}
-        />
-      )}
-
-      {modalType === MODAL_TYPES.COMPARE && (
-        <CompareCompanyModal
-          onClose={handleModalClose}
-          companies={companies}
-          handleConfirm={handleConfirm}
-          selectedCompanies={selectedCompanies}
-          pagination={pagination}
-          onSearch={setSearchKeyword}
-          onSelectCompany={handleCompanySelect}
-          onDeselectCompany={handleCompanySelect}
-          setCompanies={setCompanies}
-          fetchCompanies={fetchCompanies}
-          setSearchKeyword={setSearchKeyword}
-        />
-      )}
-
-      <div className="container">
-        <header className="header">
-          <h1 className="header-title">나의 기업을 선택해 주세요!</h1>
-        </header>
-
-        <section className="company-list">
-          {selectMyCompany ? null : (
-            <>
-              <button
-                className="add-button"
-                onClick={() => handleModalOpen(MODAL_TYPES.SELECT)}
-              >
-                <img src="/img/btn_plus.png" alt="기업 추가" />
-              </button>
-              <span className="add-button-text">기업 추가</span>
-            </>
-          )}
-
-          {selectMyCompany && (
-            <div className={styles.selected_company}>
-              <div className={styles.left}>
-                <div className={styles.selected_img_box}>
-                  <img src="" alt="" />
-                </div>
-                <span>{selectMyCompany.name}</span>
-                <p>{selectMyCompany.category.category}</p>
-              </div>
-            </div>
-          )}
-        </section>
-
-        <footer className="footer">
-          <button
-            className="compare-button"
-            onClick={() => handleModalOpen(MODAL_TYPES.COMPARE)}
-          >
-            <span className="compare-button-text">기업 비교하기</span>
-          </button>
-        </footer>
-      </div>
-    </>
-  );
-}
-
-
   const [isCompanyAdded, setIsCompanyAdded] = useState(false); // 상태 추가
   const navigate = useNavigate();
 
@@ -140,73 +28,79 @@ export function CompareMyCompany() {
 
   function handleCompare() {
     navigate("/compareResult"); // 페이지 이동
-
   }
 
   return (
     <>
-  <div class="container">
-    {/* <header class="header">
+      <div class="container">
+        {/* <header class="header">
       <h1 class="header-title">나의 기업을 선택해 주세요!</h1>
       <p className="cancel-selection-btn">선택 취소</p>
     </header> */}
-    <header className="header">
-        <h1 className="header-title">나의 기업을 선택해 주세요!</h1>
-        {isCompanyAdded && (
-          <p
-            className="cancel-selection-btn"
-            onClick={handleRemoveCompany}
-          >
-            선택 취소
-          </p>
-        )}
-      </header>
+        <header className="header">
+          <h1 className="header-title">나의 기업을 선택해 주세요!</h1>
+          {isCompanyAdded && (
+            <p className="cancel-selection-btn" onClick={handleRemoveCompany}>
+              선택 취소
+            </p>
+          )}
+        </header>
 
-      {/* <section class="company-list">
+        {/* <section class="company-list">
             <button class="add-button">
               <img src="/img/btn_plus.png" alt="plus 버튼" width="100%"/>
             </button>
             <span class="add-button-text">기업 추가</span>
           </section>  */}
 
-      <section className="company-list">
+        <section className="company-list">
+          {isCompanyAdded ? (
+            // 기업이 추가된 경우의 화면
+            <div className="added-company">
+              <img
+                src="/img/company_logo.png"
+                alt="기업 로고"
+                className="company-logo"
+              />
+              {/* <p className="company-name">코드잇</p> */}
+            </div>
+          ) : (
+            // 기업이 추가되지 않은 경우의 화면
+            <div className="add-company-container">
+              <button className="add-button" onClick={handleAddCompany}>
+                <img src="/img/btn_plus.png" alt="plus 버튼" width="100%" />
+              </button>
+              <span className="add-button-text">기업 추가</span>
+            </div>
+          )}
+        </section>
+
         {isCompanyAdded ? (
-          // 기업이 추가된 경우의 화면
-          <div className="added-company">
-            <img
-              src="/img/company_logo.png"
-              alt="기업 로고"
-              className="company-logo"
-            />
-            {/* <p className="company-name">코드잇</p> */}
+          <div>
+            <header class="inquiry-header">
+              <h1 class="header-title">어떤 기업이 궁금하세요?</h1>
+              <button class="inquiry-add-button">기업 추가하기</button>
+            </header>
+
+            <section class="company-list">
+              <p class="inquiry-add-button-text">
+                아직 추가된 기업이 없어요, <br />
+                버튼을 눌러 기업을 추가해보세요!
+              </p>
+            </section>
           </div>
-        ) : (
-          // 기업이 추가되지 않은 경우의 화면
-          <div className="add-company-container">
-            <button className="add-button" onClick={handleAddCompany}>
-            <img src="/img/btn_plus.png" alt="plus 버튼" width="100%" />
+        ) : null}
+
+        <footer className="footer">
+          <button
+            className="compare-button"
+            onClick={handleCompare}
+            disabled={!isCompanyAdded} // 기업 추가가 안 된 경우 버튼 비활성화
+          >
+            <span className="compare-button-text">기업 비교하기</span>
           </button>
-          <span className="add-button-text">기업 추가</span>
-          </div>
-        )}
-      </section>
-
-      {isCompanyAdded ? (
-        <div>
-    <header class="inquiry-header">
-      <h1 class="header-title">어떤 기업이 궁금하세요?</h1>
-      <button class="inquiry-add-button">기업 추가하기</button>
-    </header>
-
-    <section class="company-list">
-      <p class="inquiry-add-button-text">
-        아직 추가된 기업이 없어요, <br />
-        버튼을 눌러 기업을 추가해보세요!
-      </p>
-    </section>
-    </div>) : null}
-  </div>
+        </footer>
+      </div>
     </>
-)}
-
-
+  );
+}
