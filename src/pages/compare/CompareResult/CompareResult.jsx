a37da43ca092fb2ced266524c2b73fee53f5cfe6
just-codingbaby <gocks5393@naver.com>
2024-11-21 16:03:35 +0900

    feat: 투자버튼,모달 구현

--- src/pages/compare/CompareResult/CompareResult.jsx
+++ src/pages/compare/CompareResult/CompareResult.jsx
@@ -3,13 +3,15 @@ import MySelected from './MySelected';
 import '../../../shared/css/com.css';
 import ResultConfirm from './ResultConfirm';
 import RankCompare from './RankCompare';
+import InvestmentButton from '../../Investment/InvestmentButton/InvestmentButton';
 
 export default function CompareResult() {
   return (
-    <>
+    <div className={styles.container}>
       <MySelected/>
       <ResultConfirm/>
       <RankCompare/>
-    </>
+      <InvestmentButton/>
+    </div>
   )
 }
